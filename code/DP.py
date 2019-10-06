d6610504ed47d4024990573ba514331097bf2718
Robert Cheng <bobcheng15@gmail.com>
2019-10-07 01:00:22 +0800

    added sys argv for ease of use

--- code/DP.py
+++ code/DP.py
@@ -2,7 +2,7 @@ import numpy as mp
 import Policy
 import Value
 import matplotlib.pyplot as plt
-
+import sys
 class DP:
     def __init__(self, size_row, size_col, input_matrix, terminal, reward):
         self.val = Value.Value(size_row, size_col, input_matrix)
@@ -14,7 +14,12 @@ class DP:
     def policyIteration(self):
         self.val.update(self.pol, self.reward, self.terminal)
         self.pol.greedy(self.val, self.terminal)
-    def plot(self, idx):
+    def valueIteration(self, times):
+        for i in range(0, times):
+            self.val.update_vi(self.terminal, self.reward)
+        self.pol.greedy(self.val, self.terminal)
+
+    def plot(self, argv1, argv2):
         # settings
         ax = plt.gca()
         ax.set_xlim(0, self.size_col)
@@ -33,5 +38,5 @@ class DP:
                 if self.pol.policy[i][j][3] != 0: plt.arrow(j+0.5, (self.size_row)-i-0.5, 0.3, 0, width=0.02)
 
         #plt.show()
-        plt.savefig("../icons/policy"+str(idx)+".png")
+        plt.savefig("../icons/result"+ argv1 + argv2 +".png")
         plt.close()
--- code/main.py
+++ code/main.py
@@ -1,15 +1,21 @@
 import DP
 import numpy as np
+import sys
 
 
 
 if __name__ == "__main__":
-    map = np.loadtxt("../testcase/case1.txt")
-    terminal = np.loadtxt("../testcase/terminal.txt", dtype = 'i')
+
+    map = np.loadtxt("../testcase/case" + sys.argv[1] + ".txt")
+    terminal = np.loadtxt("../testcase/terminal" + sys.argv[1] + ".txt", dtype = 'i')
     size_row, size_col = map.shape
     it = iter(terminal)
     terminal = list(zip(it, it))
     dp =DP.DP(size_row, size_col, map, terminal, -1)
-    for i in range(0, 30):
-        dp.policyIteration()
-        dp.plot(i)
+    if (sys.argv[2] == "pi"):
+        for i in range(0, 30):
+            dp.policyIteration()
+
+    else:
+        dp.valueIteration(30)
+    dp.plot(sys.argv[1], sys.argv[2])
