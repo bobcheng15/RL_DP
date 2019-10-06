80edabce9f2b790f201ed763fa85d3220ae4f084
Robert Cheng <bobcheng15@gmail.com>
2019-10-06 17:42:22 +0800

    bug fix

--- code/DP.py
+++ code/DP.py
@@ -1,6 +1,7 @@
 import numpy as mp
 import Policy
 import Value
+import matplotlib.pyplot as plt
 
 class DP:
     def __init__(self, size_row, size_col, input_matrix, terminal, reward):
@@ -8,6 +9,29 @@ class DP:
         self.pol = Policy.policy(size_row, size_col, terminal)
         self.reward = reward
         self.terminal = terminal
+        self.size_col = size_col
+        self.size_row = size_row
     def policyIteration(self):
         self.val.update(self.pol, self.reward, self.terminal)
         self.pol.greedy(self.val, self.terminal)
+    def plot(self, idx):
+        # settings
+        ax = plt.gca()
+        ax.set_xlim(0, self.size_col)
+        ax.set_ylim(0, self.size_row)
+        miloc = plt.MultipleLocator(1)
+        ax.xaxis.set_minor_locator(miloc)
+        ax.yaxis.set_minor_locator(miloc)
+        ax.grid(which='minor')
+
+        # plot policy
+        for i in range (0, self.size_row):
+            for j in range(0, self.size_col):
+                if self.pol.policy[i][j][0] != 0: plt.arrow(j+0.5, (self.size_row)-i-0.5, 0, 0.3, width=0.02)
+                if self.pol.policy[i][j][1] != 0: plt.arrow(j+0.5, (self.size_row)-i-0.5, 0, -0.3, width=0.02)
+                if self.pol.policy[i][j][2] != 0: plt.arrow(j+0.5, (self.size_row)-i-0.5, -0.3, 0, width=0.02)
+                if self.pol.policy[i][j][3] != 0: plt.arrow(j+0.5, (self.size_row)-i-0.5, 0.3, 0, width=0.02)
+
+        #plt.show()
+        plt.savefig("../icons/policy"+str(idx)+".png")
+        plt.close()
--- code/main.py
+++ code/main.py
@@ -1,14 +1,15 @@
 import DP
 import numpy as np
 
-# def main():
-map = np.loadtxt("../testcase/case1.txt")
-terminal = np.loadtxt("../testcase/terminal.txt", dtype = 'i')
-size_row, size_col = map.shape
-it = iter(terminal)
-terminal = list(zip(it, it))
-DP.DP(size_row, size_col, map, terminal, -1)    # size_row, size_col = map.shape
-    # for i in range(0, size_row):
-    #     for j in range(0, size_col):
-    #         if ()
-    # DP = DP.DP(size_row, size_col, map, )
+
+
+if __name__ == "__main__":
+    map = np.loadtxt("../testcase/case1.txt")
+    terminal = np.loadtxt("../testcase/terminal.txt", dtype = 'i')
+    size_row, size_col = map.shape
+    it = iter(terminal)
+    terminal = list(zip(it, it))
+    dp =DP.DP(size_row, size_col, map, terminal, -1)
+    for i in range(0, 30):
+        dp.policyIteration()
+        dp.plot(i)
